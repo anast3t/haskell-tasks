91a674414e5682f4f23b4e6a576b8f37ac120a8f
anast3t <bortnikov20000@mail.ru>
2019-10-29 00:11:03 +0700

    solved problems

--- lists.hs
+++ lists.hs
@@ -1,22 +1,31 @@
 myhead:: [a] -> a
-myhead [] = error "Нет элементов!"
-myhead (x:xs) = x
+myhead [] = error "No elements!"
+myhead (x:_) = x
 
 mytail:: [a] -> [a]
-mytail [] = error "Нет элементов!"
-mytail (x:xs) = xs
+mytail [] = error "No elements!"
+mytail (_:xs) = xs
 
-mylength:: [a] -> Int
-mylength [] = error "Нет элементов!"
-mylength (x:xs) = 1 + mylength xs
+myFoldr:: (a -> b -> a) -> a -> [b] -> a
+myFoldr f accum [] = accum 
+myFoldr f accum (x:xs) = myFoldr f accum xs `f` x -- @машинописный обратный апостроф@, код &#96; (шестнадцатеричный U+0060) Как его вообще предпологается втыкать?
+
+myFoldl:: (a -> b -> a) -> a -> [b] -> a
+myFoldl f accum [] = accum 
+myFoldl f accum (x:xs) = myFoldl f (f accum x) xs
+
+mylength:: [a] -> Integer
+mylength [] = 0
+mylength xs = myFoldl (\x y -> x + 1) 0 xs
 
 myappend:: [a] -> a -> [a]
-myappend [] _ = error "Нет элементов!"
-myappend xs x = xs ++ [x]
+myappend [] n = [n]
+myappend (x:[]) n = x:[n]
+myappend (x:xs) n = x:myappend xs n
 
 myconcat:: [a] -> [a] -> [a]
-myconcat [] [] = error "Нет элементов!"
-myconcat xs ys = xs++ys
+myconcat (x:[]) ys = x:ys
+myconcat (x:xs) ys = x:myconcat xs ys
 
 mynull:: [a] -> Bool
 mynull [] = True
@@ -27,73 +36,54 @@ myelem [] _ = False
 myelem (x:xs) n = if x == n then True else myelem xs n
 
 mylast:: [a] -> a
-mylast [] = error "Нет элементов!"
-mylast (x:[]) = x
+mylast [] = error "No elements!"
+mylast [x] = x
 mylast (x:xs) = mylast xs
 
 myinit:: [a] -> [a]
-myinit [] = error "Нет элементов!"
+myinit [] = error "No elements!"
 myinit (x:[]) = []
 myinit (x:xs) = x : myinit xs
 
-myreverse:: [a] -> [a]
-myreverse [] = error "Нет элементов!"
-myreverse (x:[]) = [x]
-myreverse (x:xs) = myreverse xs ++ [x]
+myreverse:: [a] -> [a] 
+myreverse [] = error "No elements!"
+myreverse xs = myFoldl (\m k -> k:m) [] xs --не до конца понимаю почему это работает, тк аккумулятор фолдла не может же быть списком???
 
 myMap:: (a->b) -> [a] -> [b]
-myMap _ [] = error "Нет элементов!"
-myMap func (x:[]) = [func x]
+myMap _ [] = []
 myMap func (x:xs) = func x : myMap func xs
 
-myzip::[Int]->[Int]->[(Int,Int)]
-myzip [] [] = error "Нет элементов!"
-myzip (x:_) [] = error "Нет элементов во втором списке!"
-myzip [] (y:_) = error "Нет элементов в первом списке!"
-myzip (x:[]) (y:[]) = [(x,y)]
-myzip (x:xs) (y:[]) = [(x,y)]
-myzip (x:[]) (y:ys) = [(x,y)]
+myzip::[a]->[b]->[(a,b)]
+myzip [] [] = []
+myzip (x:_) [] = error "Length of strings are not equal! Second string is shorter."
+myzip [] (y:_) = error "Length of strings are not equal! First string is shorter."
 myzip (x:xs) (y:ys) = (x,y) : myzip xs ys
 
-tester::Int -> Bool --для фильтра
+
+tester::Int -> Bool --примерочный тестер для фильтра
 tester x = if x > 10 then True else False 
 
-myfilter:: (Int -> Bool) -> [Int] -> [Int]
-myfilter func [] = error "Нет элементов!" --виртуоз по костылям
-myfilter func (x:[]) = case func x of
-                        True -> [x]
-                        False -> []
-myfilter func (x:xs) = case func x of
-                        True -> x: myfilter func xs
-                        False -> myfilter func xs
+myfilter:: (a -> Bool) -> [a] -> [a]
+myfilter func [] = []
+myfilter func (x:xs) = if func x == True then x: myfilter func xs else myfilter func xs
 
---
 
-get :: [Int] -> Int -> Int
-get [] _= error "Нет элементов!"
-get (x:xs) n = case n of
-                  0 -> x
-                  otherwise -> get xs (n-1)
+get :: [a] -> Integer -> a
+get [] _= error "No elements!"
+get (x:xs) 0 = x 
+get (x:xs) n = get xs (n-1)
 
---
-
-mydrop :: [Int] -> Int -> [Int]
-mydrop [] _= error "Нет элементов!"
+mydrop :: [a] -> Integer -> [a] 
+mydrop [] _= error "No elements!"
 mydrop xs 0 = xs 
-mydrop (x:xs) n = case n of
-                  1 -> xs
-                  otherwise -> mydrop xs (n-1)
+mydrop (x:xs) 1 = xs
+mydrop (x:xs) n = mydrop xs (n-1)
 
---
+mytake :: [a] -> Integer -> [a]
+mytake [] _= error "No elements!" 
+mytake (x:xs) 1 = [x]
+mytake (x:xs) n = mytake xs (n-1)
 
-mytake :: [Int] -> Int -> [Int]
-mytake [] _= error "Нет элементов!" 
-mytake (x:xs) n = case n of
-                  1 -> [x]
-                  otherwise -> x : mytake xs (n-1)
-
---
-
-mysplitAt :: [Int] -> Int -> ([Int], [Int])
-mysplitAt [] _= error "Нет элементов!" 
+mysplitAt :: [a] -> Integer -> ([a], [a])
+mysplitAt [] _= error "No elements!" 
 mysplitAt xs n = (mytake xs n, mydrop xs n)
