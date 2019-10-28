068bd0c764febbae9d1a059a75bbbce4d27c53a0
anast3t <bortnikov20000@mail.ru>
2019-10-29 00:24:32 +0700

    fixed

--- 3.hs
+++ 3.hs
@@ -1,4 +1,17 @@
-myMap :: (a -> b) -> [a] -> [b]
+myMapR :: (a -> b) -> [a] -> [b]
+myMapR f xs = myFoldr (\s x -> f x:s) [] xs
 
-myMap f s = foldr func [] s where 
-    func a b = (\x xs -> f x : xs) a b
+myMapL :: (a -> b) -> [a] -> [b]
+myMapL f xs = myreverse (myFoldl (\s x -> f x:s) [] xs)
+-------------------------------------------
+myFoldr:: (a -> b -> a) -> a -> [b] -> a
+myFoldr f accum [] = accum 
+myFoldr f accum (x:xs) = myFoldr f accum xs `f` x
+
+myFoldl:: (a -> b -> a) -> a -> [b] -> a
+myFoldl f accum [] = accum 
+myFoldl f accum (x:xs) = myFoldl f (f accum x) xs
+
+myreverse:: [a] -> [a] 
+myreverse [] = error "No elements!"
+myreverse xs = myFoldl (\m k -> k:m) [] xs
