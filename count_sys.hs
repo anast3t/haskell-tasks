91de856ffeb29c38549a67b8a9340cf035168f6d
anast3t <bortnikov20000@mail.ru>
2019-12-23 16:50:05 +0700

    Add files via upload

--- count_sys.hs
+++ count_sys.hs
@@ -1,60 +1,65 @@
 import Data.Char
-
-conv:: [Char] -> [Int]
+--READY
+conv:: [Char] -> [Int] --преобразование всей строки
 conv str = map func str where
-    func x  | ((ox >= 48) && (ox <= 57)) = (ox - 48)
-            | ((ox >= 97) && (ox <= 122)) = (ox - 87)
-            | ((ox >= 65) && (ox <= 90)) = (ox - 29) 
+    func x  | ((ox >= ord '0') && (ox <= ord '9')) = (ox - 48)
+            | ((ox >= ord 'a') && (ox <= ord 'z')) = (ox - 87)
+            | ((ox >= ord 'A') && (ox <= ord 'Z')) = (ox - 29) 
             | otherwise = error "String not correct" where 
                 ox = ord x
---
-conv1:: Char -> Int
+--READY
+conv1:: Char -> Int --преобразование одного символа
 conv1 m = func m where
-    func x  | ((ox >= 48) && (ox <= 57)) = (ox - 48)
-            | ((ox >= 97) && (ox <= 122)) = (ox - 87)
-            | ((ox >= 65) && (ox <= 90)) = (ox - 29) 
+    func x  | ((ox >= ord '0') && (ox <= ord '9')) = (ox - 48)
+            | ((ox >= ord 'a') && (ox <= ord 'z')) = (ox - 87)
+            | ((ox >= ord 'A') && (ox <= ord 'Z')) = (ox - 29)
             | otherwise = error "Digit not correct" where 
                 ox = ord x
---
-dECconvint:: [Int] -> Int
+--READY
+dECconvint:: [Int] -> Int --из DEC строки в DEC число
 dECconvint [x] = x
 dECconvint (x:xs) = x*(10^(length xs)) + dECconvint xs
---
-bconv:: [Int] -> [Char]
-bconv str = map func str where
+--READY
+backConv:: [Int] -> [Char] --из числовой строки в символьную 
+backConv str = map func str where
     func x  | ((x >= 0) && (x <= 9)) = chr (x + 48)
             | ((x >= 10) && (x <= 35)) = chr (x + 87)
             | ((x >= 36) && (x <= 61)) = chr (x + 29) 
             | otherwise = error "String not correct"
---
-chkbase:: Int -> Bool
-chkbase base| ((base >= 1) && (base <= 62)) = True
+--READY
+chkbase:: Int -> Int --проверка базы
+chkbase base| ((base >= 1) && (base <= 62)) = base
             | otherwise = error "Base not correct" 
---
-checker:: Int -> [Char] -> [Char]
-checker base str = (filter (\x -> (if ((conv1 x) >= base) then error "String numbers are bigger then base (Incorrect string input)" else False)) str)
---
-checker1:: [Char] -> [Char]
-checker1 str = (filter (\x -> (if ((conv1 x) > 1) then error "String numbers are bigger then base (Incorrect string input)" else False)) str)
----------------------------------------------------------------
-toDecimal:: Int -> [Char] -> [Char]
-toDecimal 1 str = if (checker1 str == []) then show(todec2 1 str) else error "0_0" --don't beat me pls :(
-toDecimal base str = if chkbase base then (if (checker base str == []) then show (todec2 base str) else error "0_0") else error "0_0" 
+--READY
+checker:: Int -> [Char] -> [Char] --проверка всех значений (вылезают ли они за базу)
+checker base str = filter (\x -> (if ((conv1 x) >= base) then error "String numbers are bigger then base (Incorrect string input)" else True)) str
+--READY
+checker1:: [Char] -> [Char] --проверка значений с базой 1
+checker1 str = filter (\x -> (if ((conv1 x) /= 1) then error "String numbers are bigger then base (Incorrect string input)" else True)) str
 
-todec2:: Int -> [Char] -> Int
-todec2 1 [x] = ((conv1 x) - 1)
+
+------------------------------КОНЕЦ СЛУЖЕБНЫХ ФУНКЦИЙ--------------------------------- 
+
+
+toDecimal:: Int -> [Char] -> [Char] --преобразование в DEC по базе
+toDecimal 1 str = show $todec2 1 (checker1 str)
+toDecimal base str = show $todec2 (chkbase base) $checker (chkbase base) str
+
+todec2:: Int -> [Char] -> Int --вспомогательная --корректировка на фото
+todec2 1 xs = length (conv xs)
 todec2 base [x] = (conv1 x)
-todec2 base (x:xs) = (conv1 x)*(base^(length xs)) + todec2 base xs
+--todec2 base (x:xs) = (conv1 x)*(base^(length xs)) + todec2 base xs
+todec2 base xs = (conv1 (last xs)) + base*(todec2 base (init xs))
 ---------------------------------------------------------------
-fromDecimal:: Int -> [Char] -> [Char]
-fromDecimal base xs = if chkbase base then (if (checker 10 xs == []) then (bconv(frd base (dECconvint(conv xs)))) else error "0_0") else error "0_0"
+fromDecimal:: Int -> [Char] -> [Char] --преобразование из десятичной
+fromDecimal base xs = backConv $frd (chkbase base) $dECconvint $conv $checker 10 xs
 
+--вспомогательная
 frd::Int -> Int -> [Int]
-frd 1 0 = [1]
-frd 1 x = 1:frd 1 (x-1)
+frd 1 x = replicate x 1
 frd base x = if x >= base then (frd base (x `div` base) ++ [x `mod` base]) else [x]
 ---------------------------------------------------------------
 convertFromTo:: Int -> Int -> [Char] -> [Char]
 convertFromTo baseF baseT str = fromDecimal baseT (toDecimal baseF str)
 ---------------------------------------------------------------
---все вылезающие функции потом причешу (время три утра я в щи) :( 
+--поправить все невозможные ошибки
